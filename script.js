dba0682e1b6e08f0df904470a30d4bb8c5925306
A Doubt <adoubt2@gmail.com>
2022-01-19 02:39:33 +0100

    Functionality, CSS added. Bugs fixed (hopefully)
    The following changes have been made:
    1. Added a 'reset' button and a '%' button that divides by 100.
    2. Rewrote the HTML
    3. Added CSS styling and some quality of life
    4. Bugs fixed.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+//global variables
+let leftOperand = null;
+let rightOperand = null;
+let operand = '0';
+let operator = null;
+let result = null;
+let lastAction = null;
 
-
+//display
 const calculatorDisplay = document.querySelector('.calculator-display');
-calculatorDisplay.textContent = 'Here lies the mighty display of the calculator.';
+calculatorDisplay.textContent = operand;
 
 // main buttons
 const dotButton = document.querySelector('#dot-button');
@@ -22,81 +29,88 @@ const multiplyButton = document.querySelector('#multiply-button');
 const subtractButton = document.querySelector('#subtract-button');
 const addButton = document.querySelector('#add-button');
 const equalsButton = document.querySelector('#equals-button');
+const resetButton = document.querySelector('#reset-button');
+const percentButton = document.querySelector('#percent-button');
 
-let leftOperand = null;
-let rightOperand = null;
-let operand = '0';
-let operator = null;
-let result = null;
-let lastAction = null;
+
 
 
 // main buttons event listeners
 dotButton.addEventListener('click', () => {
 	if (lastAction == 'equals') operand = '0'; // resets the operand when last result was obtained by '=' sign
-	if (operand.toString().includes('.')) return; // only 1 'dot' possible in a string that will be cast into a number.
+	if (operand.toString().includes('.')) return; // only 1 'dot' possible in a string that will be cast into a number
 	operand += '.';
 	if (operand != '0') {calculatorDisplay.textContent = `${Number(operand)}.`;} // to prevent showing something like '00000.2'
 	else calculatorDisplay.textContent = (operand);
-	lastAction = 'number';
+	lastAction = 'number';	// lastAction tracks last button clicked to make things work properly
 });
 zeroButton.addEventListener('click', () => {
 	if (lastAction == 'equals') operand = '0';
-	operand += '0'; 
-	calculatorDisplay.textContent = Number(operand);
+	if (operand == '0') operand = '0'; // this prevents showing two 0s on the display
+	else operand += '0';
+	calculatorDisplay.textContent = operand;
 	lastAction = 'number';
 });
 oneButton.addEventListener('click', () => {
 	if (lastAction == 'equals') operand = '0';
-	operand += '1'
+	if (operand === '0') operand = '1';	// this prevents showing two 0s on the display - same for every other number
+	else operand += '1'
 	calculatorDisplay.textContent = Number(operand);
 	lastAction = 'number';
 });
 twoButton.addEventListener('click', () => {
 	if (lastAction == 'equals') operand = '0';
-	operand += '2'
+	if (operand == '0') operand = '2';
+	else operand += '2'
 	calculatorDisplay.textContent = Number(operand);
 	lastAction = 'number';
 });
 threeButton.addEventListener('click', () => {
 	if (lastAction == 'equals') operand = '0';
-	operand += '3';
+	if (operand == '0') operand = '3';
+	else operand += '3';
 	calculatorDisplay.textContent = Number(operand);
 	lastAction = 'number';
 });
 fourButton.addEventListener('click', () => {
 	if (lastAction == 'equals') operand = '0';
-	operand += '4';
+	if (operand == '0') operand = '4';
+	else operand += '4'
 	calculatorDisplay.textContent = Number(operand);
 	lastAction = 'number';
 });
 fiveButton.addEventListener('click', () => {
 	if (lastAction == 'equals') operand = '0';
-	operand += '5';
+	if (operand == '0') operand = '5';
+	else operand += '5'
 	calculatorDisplay.textContent = Number(operand);
 	lastAction = 'number';
 });
 sixButton.addEventListener('click', () => {
 	if (lastAction == 'equals') operand = '0';
-	operand += '6';
+	if (operand == '0') operand = '6';
+	else operand += '6'
 	calculatorDisplay.textContent = Number(operand);
 	lastAction = 'number';
 });
 sevenButton.addEventListener('click', () => {
 	if (lastAction == 'equals') operand = '0';
-	operand += '7';
+	if (operand == '0') operand = '7';
+	else operand += '7'
 	calculatorDisplay.textContent = Number(operand);
 	lastAction = 'number';
 });
 eightButton.addEventListener('click', () => {
 	if (lastAction == 'equals') operand = '0';
-	operand += '8';
+	if (operand == '0') operand = '8';
+	else operand += '8'
 	calculatorDisplay.textContent = Number(operand);
 	lastAction = 'number';
 });
 nineButton.addEventListener('click', () => {
 	if (lastAction == 'equals') operand = '0';
-	operand += '9';
+	if (operand == '0') operand = '9';
+	else operand += '9'
 	calculatorDisplay.textContent = Number(operand);
 	lastAction = 'number';
 });
@@ -105,8 +119,9 @@ nineButton.addEventListener('click', () => {
 // operator buttons event listeners
 divideButton.addEventListener('click', () => {
 	if (leftOperand != null && lastAction == 'number') {
-		result = operate(leftOperand, operand, operator);
+		result = (Math.round(1000000000 * operate(leftOperand, operand, operator)) / 1000000000); // to prevent overflowing the display
 		if (result == 'Infinity') calculatorDisplay.textContent = "Nice try, mate.";
+		else if (isNaN(result)) calculatorDisplay.textContent = 'ERR';
 		else calculatorDisplay.textContent = result;
 		leftOperand = result;
 		lastAction = 'operator';
@@ -118,7 +133,7 @@ divideButton.addEventListener('click', () => {
 });
 multiplyButton.addEventListener('click', () => {
 	if (leftOperand != null && lastAction == 'number') {
-		result = operate(leftOperand, operand, operator);
+		result = (Math.round(1000000000 * operate(leftOperand, operand, operator)) / 1000000000);
 		calculatorDisplay.textContent = result;
 		leftOperand = result;
 		lastAction = 'operator';
@@ -152,32 +167,46 @@ addButton.addEventListener('click', () => {
 	operand = '0';
 	lastAction = 'operator';
 });
-
+(Math.round(1000000000 * operate(leftOperand, operand, operator)) / 1000000000)
 
 equalsButton.addEventListener('click', () => {
 	if (!operator) return;
 	if (lastAction == 'equals') {
-		result = operate(operand, rightOperand, operator);
-		calculatorDisplay.textContent = result;
+		result = Math.round(1000000000 * operate(operand, rightOperand, operator)) / 1000000000;
+		if (result == 'Infinity') calculatorDisplay.textContent = "Nice try, mate.";
+		else calculatorDisplay.textContent = result;
 		operand = result;
 	}
 	else {
 		leftOperand = Number(leftOperand);
 		rightOperand = Number(operand);
 	
-		result = operate(leftOperand, rightOperand, operator);
+		result = Math.round(1000000000 * operate(leftOperand, rightOperand, operator)) / 1000000000;
 		leftOperand = null;
 		operand = result;
 	
 		if (result == 'Infinity') calculatorDisplay.textContent = "Nice try, mate.";
+		else if (isNaN(result)) calculatorDisplay.textContent = 'ERR';
 		else calculatorDisplay.textContent = result;
 		lastAction = 'equals';
 	}
 	
 });
 
+resetButton.addEventListener('click', () => {
+	leftOperand = null;
+	rightOperand = null;
+	operand = '0';
+	operator = null;
+	result = null;
+	lastAction = null;
+	calculatorDisplay.textContent = operand;
+});
 
-
+percentButton.addEventListener('click', () => {
+	operand = operand / 100;
+	calculatorDisplay.textContent = operand;
+});
 
 function operate (leftOperand, rightOperand, operator) {
 	switch (operator) {
@@ -192,37 +221,18 @@ function operate (leftOperand, rightOperand, operator) {
 	}
 }
 
-window = document.querySelector('window');
-window.addEventListener('click', () => {
-	console.log(`leftOperand: ${leftOperand}`);
-	console.log(`rightOperand: ${rightOperand}`)
-	console.log(`operand: ${operand}`);
-	console.log(`result: ${result}`);
-	console.log('');
-})
+const allOperationButtons = document.getElementById('operation-buttons');
 
-
-
-
-// Maybe I'll use them later
-// function add(x, y) {
-// 	return x + y;
-// }
-// function subtract(x, y) {
-// 	return x - y;
-// }
-// function multiply(x, y) {
-// 	return x * y;
-// }
-// function divide(x, y) {
-// 	return x / y;
-// }
-
-// let a = '+';
-// let b = '-';
-// let c = '*';
-// let d = '/';
-
-	// console.log(`leftOperand: ${leftOperand}`);
-	// console.log(`operand: ${operand}`);
-	// console.log(`result: ${result}`);
+allOperationButtons.addEventListener('click', (event) => {
+	const isButton = event.target.nodeName === 'BUTTON';
+	if (!isButton || event.target.id === 'equals-button') {
+	  return;
+	}
+	// removing active class from all buttons
+	addButton.classList.remove('active-operator');
+	subtractButton.classList.remove('active-operator');
+	multiplyButton.classList.remove('active-operator');
+	divideButton.classList.remove('active-operator');
+	// adding class to the active operator button
+	event.target.classList.add('active-operator');
+  })
